739525a94451d7386644e99ebef9c161934da26d
evgeni <darmanchev.evgeni@gmail.com>
2026-03-18 13:01:00 +0200

    feat authorization

--- app/server/core/config.py
+++ app/server/core/config.py
@@ -6,6 +6,9 @@ class Settings(BaseSettings):
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
     class Config:
         env_file = ".env"
